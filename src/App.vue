0cfa8f5c891b5ed5bbdd4d0158044c584fb29268
unknown <asmaa3107@gmail.com>
2022-10-31 18:43:41 +0200

    adding footer

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,18 @@
 
 <template>
   <div>Hi</div>
-  <Header/>
+  <Header />
+  <Footer />
 </template>
 
 <script>
 import Header from './components/Header'
+import Footer from './components/Footer'
 export default {
   name: 'App',
   components: {
-    Header
+    Header,
+    Footer
   }
 }
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -10,3 +10,11 @@ export default {
 }
 
 </script>
+
+<style scoped>
+header{
+ display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;}
+</style>
